e2e17be90dfed54ab771af96c2fa80df89798c4b
christon88 <c.a.onsager@gmail.com>
2017-08-30 07:59:36 +0200

    Adds fmt

--- 02_package/main/main.go
+++ 02_package/main/main.go
@@ -1,11 +1,11 @@
 package main
 
-import(
-  "fmt"
-  "github.com/christon88/GoTraining/02_package/stringutil"
+import (
+	"fmt"
+	"github.com/christon88/GoTraining/02_package/stringutil"
 )
 
 func main() {
-    fmt.Println(stringutil.Reverse("!olleH !hO"))
-    fmt.Println(stringutil.MyName)
+	fmt.Println(stringutil.Reverse("!olleH !hO"))
+	fmt.Println(stringutil.MyName)
 }
