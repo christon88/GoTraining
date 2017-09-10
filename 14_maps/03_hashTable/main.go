870d5c5d5caeb6dc1b0b2ecc80307fe433098858
christon88 <c.a.onsager@gmail.com>
2017-09-10 14:48:41 +0200

    edits 14

--- 14_maps/03_hashTable/main.go
+++ 14_maps/03_hashTable/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -17,4 +18,28 @@ func main() {
 	scanner := bufio.NewScanner(res.Body)
 	defer res.Body.Close()
 
+	//set the split function for the scanning operation
+	scanner.Split(bufio.ScanWords)
+
+	// create slice to hold counts
+	buckets := make([]int, 12)
+
+	//loop over words
+	for scanner.Scan() {
+		n := hashBucket(scanner.Text(), 12)
+		buckets[n]++
+	}
+	fmt.Println(buckets)
+	//fmt.Println("************")
+	//for i := 28; i <= 126; i++ {
+	//fmt.Printf("%v - %c - %v \n", i, i, buckets[i])
+	//}
+}
+
+func hashBucket(word string, buckets int) int {
+	var sum int
+	for _, v := range word {
+		sum += int(v)
+	}
+	return sum % buckets
 }
