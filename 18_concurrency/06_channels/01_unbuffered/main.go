4d04fd1555190c623bf2cc2e08f44f8c445d4b75
christon88 <c.a.onsager@gmail.com>
2017-09-24 13:27:30 +0200

    Added to 18

---
+++ 18_concurrency/06_channels/01_unbuffered/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c := make(chan int)
+	//var c int
+
+	go func() {
+		for i := 0; i < 20; i++ {
+			c <- i
+			//c = i
+		}
+	}()
+
+		for {
+			fmt.Println(<-c)
+			//fmt.Println(c)
+		}
+	}()
+
+	time.Sleep(time.Second)
+}
